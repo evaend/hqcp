c9feb83de40982727060b51e37236e8e3c5f9de9
fenghui <wsfh1206@foxmail.com>
2017-08-29 17:30:09 +0800

    提交：创建查询机构范围实体类、配置文件

---
+++ src/main/java/com/phxl/hqcp/entity/SelectScope.java
@@ -0,0 +1,158 @@
+package com.phxl.hqcp.entity;
+
+import java.util.Date;
+
+import com.phxl.core.base.annotation.BaseSql;
+
+@BaseSql(tableName="TD_SELECT_SCOPE", resultName="com.phxl.hqcp.dao.SelectScopeMapper.BaseResultMap")
+public class SelectScope {
+    private String selectScopeGuid;
+
+    private Long selectOrgId;
+
+    private Long orgId;
+
+    private String pYear;
+
+    private String pYmd;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    private Date createTime;
+
+    private String createUserId;
+
+    private String createUserName;
+
+    private Date modifyTime;
+
+    private String modefileUserId;
+
+    private String modefileUserNmae;
+
+    private String tfRemark;
+
+    private String selectScopeSubType;
+
+    public String getSelectScopeGuid() {
+        return selectScopeGuid;
+    }
+
+    public void setSelectScopeGuid(String selectScopeGuid) {
+        this.selectScopeGuid = selectScopeGuid == null ? null : selectScopeGuid.trim();
+    }
+
+    public Long getSelectOrgId() {
+        return selectOrgId;
+    }
+
+    public void setSelectOrgId(Long selectOrgId) {
+        this.selectOrgId = selectOrgId;
+    }
+
+    public Long getOrgId() {
+        return orgId;
+    }
+
+    public void setOrgId(Long orgId) {
+        this.orgId = orgId;
+    }
+
+    public String getpYear() {
+        return pYear;
+    }
+
+    public void setpYear(String pYear) {
+        this.pYear = pYear == null ? null : pYear.trim();
+    }
+
+    public String getpYmd() {
+        return pYmd;
+    }
+
+    public void setpYmd(String pYmd) {
+        this.pYmd = pYmd == null ? null : pYmd.trim();
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getCreateUserId() {
+        return createUserId;
+    }
+
+    public void setCreateUserId(String createUserId) {
+        this.createUserId = createUserId == null ? null : createUserId.trim();
+    }
+
+    public String getCreateUserName() {
+        return createUserName;
+    }
+
+    public void setCreateUserName(String createUserName) {
+        this.createUserName = createUserName == null ? null : createUserName.trim();
+    }
+
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+
+    public String getModefileUserId() {
+        return modefileUserId;
+    }
+
+    public void setModefileUserId(String modefileUserId) {
+        this.modefileUserId = modefileUserId == null ? null : modefileUserId.trim();
+    }
+
+    public String getModefileUserNmae() {
+        return modefileUserNmae;
+    }
+
+    public void setModefileUserNmae(String modefileUserNmae) {
+        this.modefileUserNmae = modefileUserNmae == null ? null : modefileUserNmae.trim();
+    }
+
+    public String getTfRemark() {
+        return tfRemark;
+    }
+
+    public void setTfRemark(String tfRemark) {
+        this.tfRemark = tfRemark == null ? null : tfRemark.trim();
+    }
+
+    public String getSelectScopeSubType() {
+        return selectScopeSubType;
+    }
+
+    public void setSelectScopeSubType(String selectScopeSubType) {
+        this.selectScopeSubType = selectScopeSubType == null ? null : selectScopeSubType.trim();
+    }
+}
